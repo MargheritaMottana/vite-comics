fc9387fa15f2939271be824b41907f38d6a7a7d3
MargheritaMottana <margherita.mottana@gmail.com>
2024-09-10 18:04:11 +0200

    - Creazione cartella con tutti i file collegati - inizio impostazione delle sezioni in <template> di App.vue

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script>
+
+export default {
+
+  data() {
+    return {
+      message: 'Hello world'
+    }
+  },
+
+  methods:{
+
+  },
+}
+
+</script>
+
+<template>
+
+  <header>
+
+  </header>
+
+  <main>
+    <!-- content -->
+    <section>
+        <div>
+          {{ message }}
+        </div>
+    </section>
+
+    <!-- prodotti -->
+    <section>
+      
+    </section>
+
+  </main>
+
+  <footer>
+
+    <!-- footer top -->
+    <section>
+
+    </section>
+
+    <!-- footer bottom -->
+    <section>
+      
+    </section>
+
+  </footer>
+
+</template>
+
+<style scoped>
+
+</style>
